4a2c42e25101f8e70ae378b112a32a11fa08f47e
Bhavesh Chandewal <55232014+Bhaveshchandewal@users.noreply.github.com>
2020-05-27 02:52:33 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'BuddiesGram',
+      title: 'BuzzWorld',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         scaffoldBackgroundColor: Colors.black,
